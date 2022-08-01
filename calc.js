a5f6ef2cc4108be22625821a7f6c5cc48e6e9b2d
Shivam <thisisshivam06@gmail.com>
2022-08-01 23:41:49 +0530

    first commit

---
+++ calc.js
@@ -0,0 +1,11 @@
+// export function calc() {
+//   console.log("Module code executed");
+// }
+
+function sum(a, b) {
+  return a + b;
+}
+function cube(a) {
+  return a * a * a;
+}
+export { sum, cube };
